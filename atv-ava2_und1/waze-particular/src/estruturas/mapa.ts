a562fa2d504627d4a204ae88b0bc2b3d25a15152
Gabriel Luna <gabriel-luna@gabriel.luna-Aspire-A515-51>
2026-09-13 15:20:22 -0300

    Adiciona mapa à estrutura

--- atv-ava2_und1/waze-particular/src/estruturas/mapa.ts
+++ atv-ava2_und1/waze-particular/src/estruturas/mapa.ts
@@ -1,19 +1,36 @@
 import type { PontoMapa } from "../tipos/pontoMapa";
+import type { Conexao } from "../tipos/conexao";
 
 export class Mapa {
     private pontos: PontoMapa[] = [];
+    private conexoes: Conexao[] = [];
+
     private quantidadePontos: number = 0;
+    private quantidadeConexoes: number = 0;
 
     public adicionarPonto(ponto: PontoMapa): void {
         this.pontos[this.quantidadePontos] = ponto;
         this.quantidadePontos++;
     }
 
+    public adicionarConexao(conexao: Conexao): void {
+        this.conexoes[this.quantidadeConexoes] = conexao;
+        this.quantidadeConexoes++;
+    }
+
     public obterPonto(indice: number): PontoMapa {
         return this.pontos[indice];
     }
 
+    public obterConexao(indice: number): Conexao {
+        return this.conexoes[indice];
+    }
+
     public obterQuantidadePontos(): number {
         return this.quantidadePontos;
+    }
+
+    public obterQuantidadeConexoes(): number {
+        return this.quantidadeConexoes;
     }
 }
